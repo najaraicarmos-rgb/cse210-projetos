eac972635056549ceb2c4e81b1a9848572292de3
najaraicarmos-rgb <najaraicarmos@gmail.com>
2026-09-05 20:58:42 -0300

    conclui exercicio2 s1

--- semana01/Exercicio2/Program.cs
+++ semana01/Exercicio2/Program.cs
@@ -4,6 +4,64 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Olá, Mundo! Este é o Projeto Exercicio2.");
+        Console.Write("Qual é a porcentagem da sua nota? ");
+        string entrada = Console.ReadLine();
+        int percentual = int.Parse(entrada);
+
+        string letra = "";
+
+        if (percentual >= 90)
+        {
+            letra = "A";
+        }
+        else if (percentual >= 80)
+        {
+            letra = "B";
+        }
+        else if (percentual >= 70)
+        {
+            letra = "C";
+        }
+        else if (percentual >= 60)
+        {
+            letra = "D";
+        }
+        else
+        {
+            letra = "F";
+        }
+
+        string sinal = "";
+        int ultimoDigito = percentual % 10;
+
+        if (ultimoDigito >= 7)
+        {
+            sinal = "+";
+        }
+        else if (ultimoDigito < 3)
+        {
+            sinal = "-";
+        }
+
+        if (letra == "A" && sinal == "+")
+        {
+            sinal = "";
+        }
+        else if (letra == "F")
+        {
+            sinal = "";
+        }
+
+        Console.WriteLine();
+        Console.WriteLine($"Sua nota conceitual é: {letra}{sinal}");
+
+        if (percentual >= 70)
+        {
+            Console.WriteLine("Parabéns! Você foi aprovado no curso!");
+        }
+        else
+        {
+            Console.WriteLine("Não foi desta vez, mas continue se esforçando para a próxima!");
+        }
     }
 }
