18a66966df7bd68b70128f0f43131af43eab1f2d
najaraicarmos-rgb <najaraicarmos@gmail.com>
2026-09-05 21:11:27 -0300

    conclui exercico 3 s1

--- semana01/Exercicio3/Program.cs
+++ semana01/Exercicio3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Olá, Mundo! Este é o Projeto Exercicio3.");
+        Random geradorAleatorio = new Random();
+        int numeroMagico = geradorAleatorio.Next(1, 101);
+
+        int palpite = -1;
+
+        while (palpite != numeroMagico)
+        {
+            Console.Write("Qual é o seu palpite? ");
+            palpite = int.Parse(Console.ReadLine());
+
+            if (numeroMagico > palpite)
+            {
+                Console.WriteLine("Mais alto");
+            }
+            else if (numeroMagico < palpite)
+            {
+                Console.WriteLine("Mais baixo");
+            }
+            else
+            {
+                Console.WriteLine("Você adivinhou!");
+            }
+        }
     }
 }
