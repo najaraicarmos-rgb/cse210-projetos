7e265fbbfb334f539155737e9e4bdf35cbe66914
najaraicarmos-rgb <najaraicarmos@gmail.com>
2026-09-05 21:34:30 -0300

    exercicio 5 s1 concluido

--- semana01/Exercicio5/Program.cs
+++ semana01/Exercicio5/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Olá, Mundo! Este é o Projeto Exercicio5.");
+        ExibirBoasVindas();
+
+        string nomeUsuario = PerguntarNomeUsuario();
+        int numeroFavorito = PerguntarNumeroFavorito();
+
+        int quadrado = ElevarAoQuadrado(numeroFavorito);
+
+        ExibirResultado(nomeUsuario, quadrado);
+    }
+
+    static void ExibirBoasVindas()
+    {
+        Console.WriteLine("Bem-vindo ao programa!");
+    }
+
+    static string PerguntarNomeUsuario()
+    {
+        Console.Write("Por favor, insira seu nome: ");
+        string nome = Console.ReadLine();
+        return nome;
+    }
+
+    static int PerguntarNumeroFavorito()
+    {
+        Console.Write("Por favor, insira seu número favorito: ");
+        int numero = int.Parse(Console.ReadLine());
+        return numero;
+    }
+
+    static int ElevarAoQuadrado(int numero)
+    {
+        int resultado = numero * numero;
+        return resultado;
+    }
+
+    static void ExibirResultado(string nome, int quadrado)
+    {
+        Console.WriteLine($"{nome}, o quadrado do seu número é {quadrado}");
     }
 }
